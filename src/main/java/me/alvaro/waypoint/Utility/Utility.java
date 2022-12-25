58c37c76f14b375dbde06588d070f05853d7fe58
alvarosoaress <alvarosoares01@gmail.com>
2022-12-25 01:26:52 -0300

    plugin jar

--- src/main/java/me/alvaro/waypoint/Utility/Utility.java
+++ src/main/java/me/alvaro/waypoint/Utility/Utility.java
@@ -1,18 +1,21 @@
 package me.alvaro.waypoint.Utility;
 
+import com.google.gson.JsonObject;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Utility {
-    public boolean isNum(String[] args, Player p) {
+    public static boolean isNum(String[] args, Player p) {
         for (int i = 1; i < 4; i++) {
             try {
                 Double.parseDouble(args[i]);
@@ -24,24 +27,88 @@ public class Utility {
         return true;
     }
 
-    public void addJson(int x, int y, int z, World w) throws IOException, ParseException {
-        FileWriter file = new FileWriter("plugins//coordsList.json", false);
+    // verificar se o arquivo JSON está vazio
+    // se estiver vazio irá escrever um  []
+    // para poder funcionar
+    // motivo? eu não faço ideia, só sei q funciona
+    public static void verifyJSON(String path) throws IOException {
+        FileWriter file = new FileWriter(path, true);
+        FileReader reader = new FileReader(path);
+
+        if (reader.read() == -1) {
+            file.write("[]");
+            file.close();
+        }
+    }
+
+    // função para adicionar coordenada ao JSON
+    public static void addJson(String nome, int x, int y, int z, World w) throws IOException {
+        verifyJSON("plugins//coordsList.json");
+        FileWriter file = new FileWriter("plugins//coordsList.json", true);
 
         JSONParser parser = new JSONParser();
-        JSONObject coordsList = (JSONObject) parser.parse(new FileReader("plugins//coordsList.json"));
+        try {
+            JSONArray coordsList = (JSONArray) parser.parse(new FileReader("plugins//coordsList.json"));
 
-        JSONObject coords = new JSONObject();
+            JSONObject coords = new JSONObject();
 
-        coords.put("X", x);
-        coords.put("Y", y);
-        coords.put("Z", z);
-        coords.put("W", w.toString());
+            coords.put("Nome", nome);
+            coords.put("X", x);
+            coords.put("Y", y);
+            coords.put("Z", z);
+            coords.put("W", w.toString());
+            coords.put("ID", coordsList.size());
 
-        coordsList.put(coordsList.size()+1, coords);
+            coordsList.add(coords);
 
-        file.write(coordsList.toJSONString());
+            FileOutputStream writer = new FileOutputStream("plugins//coordsList.json");
+            writer.write(("").getBytes());
 
-        file.close();
-        System.out.println("JSON Criado e atualizado");
+            file.write(coordsList.toJSONString());
+
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void printCoords(Player p, JSONArray coordsList) {
+        for (Object obj: coordsList) {
+            JSONObject coords = (JSONObject) obj;
+            p.sendMessage(ChatColor.WHITE + "-".repeat(coords.get("Nome").toString().length() * 3));
+            p.sendMessage(ChatColor.GOLD + ChatColor.BOLD.toString() + "Nome: " + coords.get("Nome").toString());
+            p.sendMessage(ChatColor.RED + ChatColor.BOLD.toString() + "X: " + coords.get("X").toString());
+            p.sendMessage(ChatColor.GREEN + ChatColor.BOLD.toString() + "Y: " + coords.get("Y").toString());
+            p.sendMessage(ChatColor.BLUE + ChatColor.BOLD.toString() + "Z: " + coords.get("Z").toString());
+            p.sendMessage(ChatColor.BLUE + ChatColor.BOLD.toString() + "Mundo: " + coords.get("W").toString());
+            p.sendMessage(ChatColor.WHITE + "-".repeat(coords.get("Nome").toString().length() * 3));
+        }
+
+    }
+
+    public static boolean readJson(Player p) throws IOException {
+        FileWriter file = new FileWriter("plugins//coordsList.json", true);
+
+        FileReader reader = new FileReader("plugins//coordsList.json");
+
+        if (reader.read() < 3) {
+            p.sendMessage(ChatColor.RED + ChatColor.BOLD.toString() + "Ainda nenhuma coordenada foi salva!");
+            return false;
+        } else {
+            JSONParser parser = new JSONParser();
+            try {
+                JSONArray coordsList = (JSONArray) parser.parse(new FileReader("plugins//coordsList.json"));
+                printCoords(p, coordsList);
+
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return true;
     }
 }
--- src/main/java/me/alvaro/waypoint/commands/WaypointAdd.java
+++ src/main/java/me/alvaro/waypoint/commands/WaypointAdd.java
@@ -3,6 +3,7 @@ package me.alvaro.waypoint.commands;
 import me.alvaro.waypoint.Utility.Utility;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,7 +17,6 @@ public class WaypointAdd implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Utility util = new Utility();
 
         if (sender instanceof Player) {
             Player p = (Player) sender;
@@ -28,21 +28,26 @@ public class WaypointAdd implements CommandExecutor {
             } else if (args.length > 4) {
                 p.sendMessage(ChatColor.RED + ChatColor.BOLD.toString() + "Muitos argumentos para o comando!");
                 return false;
-            } else if (!(util.isNum(args, p))) {
+            } else if (!(Utility.isNum(args, p))) {
                 // testar para ver se X Y Z passados são númericos
             } else {
+                // pegando localização atual do player
                 Location loc = p.getLocation();
+//
+//                try {
+//                    Utility.addJson((int) loc.getX(), (int) loc.getY(), (int) loc.getZ(), loc.getWorld());
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
 
                 try {
-                    util.addJson((int) loc.getX(), (int) loc.getY(), (int) loc.getZ(), loc.getWorld());
+                    Utility.addJson(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]),Integer.parseInt(args[3]),loc.getWorld());
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (ParseException e) {
-                    System.out.println(e.getMessage());
                     throw new RuntimeException(e);
                 }
 
                 p.sendMessage(ChatColor.WHITE + "-".repeat(args[0].length() * 3));
+                p.sendMessage(ChatColor.GOLD + ChatColor.BOLD.toString() + "\tNova coordenada salva! ");
                 p.sendMessage(ChatColor.GOLD + ChatColor.BOLD.toString() + "Nome: " + args[0]);
                 p.sendMessage(ChatColor.RED + ChatColor.BOLD.toString() + "X: " + args[1]);
                 p.sendMessage(ChatColor.GREEN + ChatColor.BOLD.toString() + "Y: " + args[2]);
